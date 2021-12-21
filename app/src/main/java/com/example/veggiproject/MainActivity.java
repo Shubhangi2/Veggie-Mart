19ef2e50fad1f8558823462aff3f0f20a6166b6b
Shubhangi Jadhav <jshubhangi958@gmail.com>
2021-12-21 10:02:47 +0530

    customize the ui

--- app/src/main/java/com/example/veggiproject/MainActivity.java
+++ app/src/main/java/com/example/veggiproject/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
