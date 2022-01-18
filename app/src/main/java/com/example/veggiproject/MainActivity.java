c7f088e370b5f18e647347370439eb97c2f3f630
Shubhangi Jadhav <jshubhangi958@gmail.com>
2022-01-18 20:59:42 +0530

    Navigation fragments are added

--- app/src/main/java/com/example/veggiproject/MainActivity.java
+++ app/src/main/java/com/example/veggiproject/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_veggie_mart, R.id.nav_my_orders, R.id.nav_my_cart, R.id.nav_my_wishlist, R.id.nav_my_account)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
