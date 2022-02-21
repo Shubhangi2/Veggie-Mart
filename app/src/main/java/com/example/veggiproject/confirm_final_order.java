7c83c6ce4928dccfca084bcc010bd8520114a43e
Shubhangi Jadhav <jshubhangi958@gmail.com>
2022-02-21 16:37:20 +0530

    make some changes in order section of database

--- app/src/main/java/com/example/veggiproject/confirm_final_order.java
+++ app/src/main/java/com/example/veggiproject/confirm_final_order.java
@@ -16,12 +16,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
 
 public class confirm_final_order extends AppCompatActivity {
     private String final_amount;
@@ -77,6 +81,8 @@ public class confirm_final_order extends AppCompatActivity {
 
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         String currentUser = firebaseUser.getUid();
+        String currentUserEmail = firebaseUser.getEmail();
+        String order_id = "orderId " + saveCurrentDate + saveCurrentTime;
 
         HashMap<String, Object> orderMap = new HashMap<>();
         orderMap.put("totalAmount", final_amount);
@@ -86,31 +92,91 @@ public class confirm_final_order extends AppCompatActivity {
         orderMap.put("city", city.getText().toString());
         orderMap.put("date", saveCurrentDate);
         orderMap.put("time", saveCurrentTime);
+        orderMap.put("order_id", order_id);
 
-        order.child(currentUser).updateChildren(orderMap)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            FirebaseDatabase.getInstance().getReference()
-                                    .child("cartList")
-                                    .child("User view")
-                                    .child(currentUser)
-                                    .removeValue()
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if(task.isSuccessful()){
-                                                Intent intent = new Intent(confirm_final_order.this, MainActivity.class);
-                                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                                startActivity(intent);
-                                                finish();
-                                            }
-                                        }
-                                    });
-                        }
-                    }
-                });
+//        order.child(currentUser).child(order_id).updateChildren(orderMap)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if(task.isSuccessful()){
+//                            FirebaseDatabase.getInstance().getReference()
+//                                    .child("cartList")
+//                                    .child("User view")
+//                                    .child(currentUser)
+//                                    .removeValue()
+//                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                        @Override
+//                                        public void onComplete(@NonNull Task<Void> task) {
+//                                            if(task.isSuccessful()){
+//                                                Intent intent = new Intent(confirm_final_order.this, MainActivity.class);
+//                                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                                                startActivity(intent);
+//                                                finish();
+//                                            }
+//                                        }
+//                                    });
+//                        }
+//                    }
+//                });
+
+        DatabaseReference mycartItem = FirebaseDatabase.getInstance().getReference().child("cartList");
+
+        mycartItem.child("User view").child(currentUser).child("products").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Map map = (Map)snapshot.getValue();
+
+                DatabaseReference order = FirebaseDatabase.getInstance().getReference().child("Orders");
+
+                order.child(currentUser).child(order_id).child("contact details").updateChildren(orderMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    order.child(currentUser).child(order_id).child("cart details")
+                                            .updateChildren(map)
+                                            .addOnCompleteListener(new OnCompleteListener() {
+                                                @Override
+                                                public void onComplete(@NonNull Task task) {
+                                                    if(task.isSuccessful()){
+                                                        if(task.isSuccessful()){
+                                                            FirebaseDatabase.getInstance().getReference()
+                                                                    .child("cartList")
+                                                                    .child("User view")
+                                                                    .child(currentUser)
+                                                                    .removeValue()
+                                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                        @Override
+                                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                                            if(task.isSuccessful()){
+                                                                                Toast.makeText(getApplicationContext(), "Your order received successfully", Toast.LENGTH_SHORT).show();
+                                                                                Intent intent = new Intent(confirm_final_order.this,MainActivity.class);
+                                                                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                                                startActivity(intent);
+                                                                                finish();
+                                                                            }
+                                                                        }
+                                                                    });
+                                                        }
+                                                    }
+                                                }
+                                            });
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "Data is not stored", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
     }
 }
